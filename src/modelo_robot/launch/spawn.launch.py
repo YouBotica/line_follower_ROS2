a193e270e5ee5667251c6c958b5eb27b81069d20
Andres F Hoyos <andresfelipe3@hotmail.es>
2022-06-22 16:02:21 -0500

    Packages for opening a URDF in RVIZ2 with launch file

---
+++ src/modelo_robot/launch/spawn.launch.py
@@ -0,0 +1,37 @@
+import os
+  
+from ament_index_python.packages import get_package_share_directory
+ 
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+  
+def generate_launch_description():
+ 
+
+    robot_name_in_model = 'Seguidor_linea_robot'
+
+ 
+    #spawn the robot 
+    spawn = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=["-topic", "/robot_description", 
+                    "-entity", robot_name_in_model,
+                    "-x", '0.0',
+                    "-y", '0.0',
+                    "-z", '0.05',
+                    "-Y", '0.0']
+    )
+
+     
+    return LaunchDescription([
+        spawn,
+    ])
